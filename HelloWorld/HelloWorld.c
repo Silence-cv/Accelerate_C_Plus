58cc280ccf0b8a978ff84bd57f46cd67a0a546da
unknown <20173671@cqu.edu.cn>
2021-08-24 00:27:06 +0800

    On branch work  Changes to be committed: 	new file:   HelloWorld/.HelloWorld.c.un~ 	new file:   HelloWorld/HelloWorld.c

---
+++ HelloWorld/HelloWorld.c
@@ -0,0 +1,10 @@
+/******************
+ * this is the first C++ program created by syf
+ * Hello C++
+ ***********************/
+#include <iostream>
+int main()
+{
+	std::cout<<"Hello, World!"<<std::endl;
+	return 0;
+}
