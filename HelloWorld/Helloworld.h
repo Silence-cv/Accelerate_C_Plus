515d060c23af2832db27eebd330a2f3b09407232
unknown <20173671@cqu.edu.cn>
2021-09-22 00:49:39 +0800

    3rd

--- HelloWorld/Helloworld.h
+++ HelloWorld/Helloworld.h
@@ -1,10 +1,16 @@
 #ifndef HELLO_WORLD_H
 #define HELLO_WORLD_H
 
+/************
+*我喜欢谁？
+*不，我是渣男
+***********************/
+
 int a;
 int b;
 
 void func(char* str);
+void func2(char*cc);
 
 
 #endif
